3e908f3f4e8d05b8e9e502a40324ef0ef5f8ef5f
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-06 10:26:17 +0900

    initial commit

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+set -euxo pipefail
+cd "$(dirname "$0")"
+
+sudo apt update && sudo apt upgrade
+sudo apt apt install \
+    git \
+    vim
+
+# install OpenCV Dependencies
+sudo apt install \
+    libavutil56 \
+    libcairo-gobject2 \
+    libgtk-3-0 \
+    libqtgui4 \
+    libpango-1.0-0 \
+    libqtcore4 \
+    libavcodec58 \
+    libcairo2 \
+    libswscale5 \
+    libtiff5 \
+    libqt4-test \
+    libatk1.0-0 \
+    libavformat58 \
+    libgdk-pixbuf2.0-0 \
+    libilmbase23 \
+    libjasper1 \
+    libopenexr23 \
+    libpangocairo-1.0-0 \
+    libwebp6
+
+command echo 'export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1' >>~/.bashrc
+
+/usr/bin/python3 -m pip install -U
+/usr/bin/python3 -m pip install \
+    black \
+    isort \
+    opencv-python
+
+# SSH Basic Configs
+command mkdir ~/.ssh
+command touch ~/.ssh/authorized_keys
+command chmod 700 ~/.ssh
+command echo 'COPY PUB KEY TO ~/.ssh/authorized_keys'
