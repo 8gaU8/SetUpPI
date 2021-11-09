0e8aff4ff6440c15a86f547c8b50de5ea88d1de3
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-09 10:16:40 +0900

    setup.shの整理

--- setup.sh
+++ setup.sh
@@ -13,22 +13,22 @@ sudo apt install \
     libopencv-dev \
     python3-opencv
 
-command echo 'export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1' >>~/.bashrc
+echo 'export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1' >>~/.bashrc
 /usr/bin/python3 -m pip install -U pip setuptools wheel
 /usr/bin/python3 -m pip install \
     black \
     isort 
 
 # SSH Basic Configs
-command mkdir ~/.ssh
-command touch ~/.ssh/authorized_keys
-command chmod 700 ~/.ssh
-command echo 'COPY PUB KEY TO ~/.ssh/authorized_keys'
+mkdir ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+echo 'COPY PUB KEY TO ~/.ssh/authorized_keys'
 
 
 # set ~/.vimrc
 curl -sSL https://raw.githubusercontent.com/straxFromIbr/SetUpPI/main/vimrc > ./vimrc
 if ! test -e ~/.vimrc; then
-    command mv ./vimrc ~/.vimrc
+    mv ./vimrc ~/.vimrc
 fi
-command rm ./vimrc
+rm ./vimrc
